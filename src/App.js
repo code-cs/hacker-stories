44071e6ed73eee81c325777c1d9caeb0672a1785
code-cs <monireh.rasouli20@gmail.com>
2024-01-09 11:27:49 +0000

    added state

--- src/App.js
+++ src/App.js
@@ -1,59 +1,69 @@
+import { useState } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    ur: 'https://react.com',
-    author: 'Jordon Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    ur: 'https://redux.com',
-    author: 'Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      ur: 'https://react.com',
+      author: 'Jordon Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      ur: 'https://redux.com',
+      author: 'Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <div>
       <h1>My hacker story</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
 };
 
-const List = () => {
-  return (
-    <ul>
-      {list.map((item) => {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span> {item.author} </span>
-            <span> {item.num_comments} </span>
-            <span> {item.points} </span>
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span> {props.item.author} </span>
+    <span> {props.item.num_comments} </span>
+    <span> {props.item.points} </span>
+  </li>
+);
 
 const Search = () => {
+  const [searchItem, setSearchItem] = useState('');
+
+  const onChangeHandler = (event) => {
+    setSearchItem(event.target.value);
+  };
+
   return (
     <>
       <label htmlFor='search'> Search: </label>
-      <input id='search' type='text' />
+      <input id='search' type='text' onChange={onChangeHandler} />
+      <p>
+        Searching for <strong>{searchItem}</strong>
+      </p>
     </>
   );
 };
